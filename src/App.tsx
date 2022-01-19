aa8f93dea6e07683eba84b9bf3d6f79b2ebe2adf
Michael Cordeiro <62896592+michaelcordeiro@users.noreply.github.com>
2022-01-19 00:40:59 -0300

    update files

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,20 @@
+import { useState, useEffect } from 'react';
 import * as C from './App.styles';
 import { Item } from './types/Item';
-import { Category } from './types/category';
+import { Category } from './types/Category';
 import { categories } from './data/categories';
 import { items } from './data/items';
+import { getCurrentMonth, filterListByMonth } from './helpers/dateFilter';
 
 const App = () => {
+  const [list, setList] = useState(items);
+  const [filterList, setFilterList] = useState<Item[]>([]);
+  const [currentMonth, setCurrentMonth] = useState(getCurrentMonth()); 
+
+  useEffect(()=>{
+    setFilterList( filterListByMonth(list, currentMonth) );
+  }, [list, currentMonth]);
+
   return (
     <C.Container>
       <C.Header>
