ea70dcb3bd536fe49395128654afd9b12c0b1de2
Michael Cordeiro <62896592+michaelcordeiro@users.noreply.github.com>
2022-01-29 00:15:01 -0300

    files

--- src/components/TableItem/index.tsx
+++ src/components/TableItem/index.tsx
@@ -1,7 +1,18 @@
 import * as C from './styles'
+import { Item } from '../../types/Item'
 
-export const TableItem = () => {
+
+type Props = {
+    item: Item
+}
+
+export const TableItem = ({ item }: Props) => {
     return (
-
+        <C.TableLine>
+            <C.TableColumn>...</C.TableColumn>
+            <C.TableColumn>{item.category}</C.TableColumn>
+            <C.TableColumn>{item.title}</C.TableColumn>
+            <C.TableColumn> R$ {item.value}</C.TableColumn>
+        </C.TableLine> 
     );
 }
