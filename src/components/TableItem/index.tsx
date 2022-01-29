bdbc33503b05ea4f3c22365c0495f5461b2e7fc0
Michael Cordeiro <62896592+michaelcordeiro@users.noreply.github.com>
2022-01-29 00:23:41 -0300

    ...

--- src/components/TableItem/index.tsx
+++ src/components/TableItem/index.tsx
@@ -9,7 +9,7 @@ type Props = {
 export const TableItem = ({ item }: Props) => {
     return (
         <C.TableLine>
-            <C.TableColumn>...</C.TableColumn>
+            <C.TableColumn>...</C.TableColumn> 
             <C.TableColumn>{item.category}</C.TableColumn>
             <C.TableColumn>{item.title}</C.TableColumn>
             <C.TableColumn> R$ {item.value}</C.TableColumn>
--- src/components/TableItem/styles.ts
+++ src/components/TableItem/styles.ts
@@ -3,5 +3,5 @@ import styled from 'styled-components';
 export const TableLine = styled.tr``; 
 
 export const TableColumn = styled.td`
-    padding: 10px 0;
+    padding: 10px 0; 
 `;
